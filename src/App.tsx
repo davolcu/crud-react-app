d792f1c45a835330a223a278419f181872940d13
Dav <dav@MacBook-Air-de-Dav.local>
2019-10-04 08:13:08 +0200

    First commit - npm start working

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+// Out of the box imports
+import React from 'react';
+import './styles/styles.scss';
+
+const App = () => (
+    <div>
+        <h1>Hello World!</h1>
+    </div>
+);
+
+export default App;
---
+++ src/index.tsx
@@ -0,0 +1,7 @@
+// Out of th box imports
+import React from 'react';
+import ReactDOM from 'react-dom';
+// Custom imports
+import App from './App';
+
+ReactDOM.render(<App />, document.querySelector('#root'));
